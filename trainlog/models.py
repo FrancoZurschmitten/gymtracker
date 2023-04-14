b4b0ba4bed394c4705adc745890ee40464238ebe
FrancoZurschmitten <francodanielzurschmitten@gmail.com>
2023-04-14 12:25:19 -0300

    Modelos Exercise, Division, Routines, Notes, ExerciseNotes, MuscleGroup, SubMuscleGroup, ExerciseCategory. Abstract models NameInfo, CreateInfo.

--- trainlog/models.py
+++ trainlog/models.py
@@ -11,31 +11,40 @@ class NameInfo(models.Model):
         abstract = True
 
 
-class MuscleGroup(NameInfo):
+class CreateInfo(models.Model):
+    last_modified = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-        db_table = "muscle_group"
+        abstract = True
+
+
+class MuscleGroup(NameInfo, CreateInfo):
+    class Meta:
+        db_table = "muscle_groups"
         get_latest_by = "order_date"
 
     def __str__(self):
         return self.name
 
 
-class SubMuscleGroup(NameInfo):
+class SubMuscleGroup(NameInfo, CreateInfo):
     muscle_group = models.ForeignKey(MuscleGroup, on_delete=models.CASCADE)
 
     class Meta:
+        db_table = "sub_muscle_groups"
         ordering = ["muscle_group"]
 
     def __str__(self):
         return "-".join([self.name, self.muscle_group.name])
 
 
-class ExerciseCategory(NameInfo):
+class ExerciseCategory(NameInfo, CreateInfo):
     def __str__(self):
         return self.name
 
 
-class Exercise(NameInfo):
+class Exercise(NameInfo, CreateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     muscle = models.ManyToManyField(SubMuscleGroup, blank=True)
     category = models.ForeignKey(
@@ -45,69 +54,91 @@ class Exercise(NameInfo):
         null=True,
         related_name="category",
     )
-    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "exercises"
 
     def clean(self):
         super().clean()
         for sub_muscle in self.muscle.all():
             if sub_muscle.muscle_group != self.category.muscle_group:
                 raise ValidationError(
-                    f"SubMuscleGroup {sub_muscle.name} does not belong to the same MuscleGroup as the Exercise Category."
+                    f"""SubMuscleGroup {sub_muscle.name} does not belong 
+                    to the same MuscleGroup as the Exercise Category."""
                 )
 
     def __str__(self):
         return self.name
 
 
-class Division(models.Model):
+class Division(NameInfo, CreateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=500, blank=True)
-    exercises = models.ManyToManyField(Exercise, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    exercises = models.ManyToManyField(Exercise)
+
+    class Meta:
+        db_table = "divisions"
 
     def __str__(self):
         return self.name
 
 
-class Routine(models.Model):
+class Routine(NameInfo, CreateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=500, blank=True)
-    divisions = models.ManyToManyField(Division, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    divisions = models.ManyToManyField(Division)
+
+    class Meta:
+        db_table = "routines"
 
     def __str__(self):
         return self.name
 
 
-class Note(models.Model):
+class Note(CreateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
-    division = models.ForeignKey(
-        Division, on_delete=models.CASCADE, blank=True, null=True
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
+    routine = models.ForeignKey(Routine, on_delete=models.CASCADE, blank=True)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "notes"
+
+    @property
+    def exercise_notes(self):
+        return ExerciseNote.objects.filter(note=self)
 
     def __str__(self):
         return "|".join(
             [
-                self.routine.name,
+                self.division.name,
                 str(self.created_at.date()),
             ]
         )
 
 
-class ExerciseNote(models.Model):
+class ExerciseNote(CreateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     note = models.ForeignKey(Note, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    series = models.CharField(max_length=200, blank=True)
-    reps = models.CharField(max_length=200, blank=True)
-    weights = models.CharField(max_length=200, blank=True)
-    rest = models.CharField(max_length=200, blank=True)
-    annot = models.TextField(max_length=2000, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    series = models.CharField(max_length=50, blank=True)
+    reps = models.CharField(max_length=50, blank=True)
+    weights = models.CharField(max_length=50, blank=True)
+    rest = models.CharField(max_length=50, blank=True)
+    annot = models.TextField(max_length=500, blank=True)
+
+    class Meta:
+        db_table = "exercise_notes"
+
+    def clean(self) -> None:
+        super().clean()
+        if not self.exercise in self.note.division.exercises.all():
+            raise ValidationError(
+                f"""The exercise {self.exercise.name} does not belong to 
+                the note {self.note.__str__()}."""
+            )
+        elif self.user != self.note.user:
+            raise ValidationError(f"""The user does not belong to the user in note.""")
+        for exercise_note in self.note.exercise_notes.all():
+            if self.exercise == exercise_note.exercise:
+                raise ValidationError(f""""This exercise note already exists.""")
 
     def __str__(self):
         return "|".join([self.exercise.name, str(self.created_at.date())])
