c183ba4c2bd38a8a530097d53f21872600bf4264
franco <FrancoZurschmitten>
2024-02-13 20:11:18 -0300

    config for production

---
+++ backend/core/settings/prod.py
@@ -0,0 +1,29 @@
+from .common import *
+
+
+########
+# CORE #
+########
+
+DEBUG = False
+
+ALLOWED_HOSTS = [".vercel.app"]
+
+WSGI_APPLICATION = "core.wsgi.app"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "URL": SECRETS["POSTGRES_URL"],
+        "USER": SECRETS["POSTGRES_USER"],
+        "HOST": SECRETS["POSTGRES_HOST"],
+        "PASSWORD": SECRETS["POSTGRES_PASSWORD"],
+        "NAME": SECRETS["POSTGRES_DATABASE"],
+    }
+}
+
+CORS_ALLOWED_ORIGINS = ["https://www.gymtracker-app.vercel.com"]
+
+SIMPLE_JWT = {
+    "SIGNING_KEY": SECRETS["JWT_SECRET_KEY"],
+}
